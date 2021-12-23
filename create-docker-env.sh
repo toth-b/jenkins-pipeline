2ee45c98ae4f9e3ac7a0e855654813ac31577a61
toth-b <tothbalazs.contact@gmail.com>
2021-12-23 14:59:28 +0100

    Script to create docker network and container for Jenkins

---
+++ create-docker-env.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+# Create a docker network for Jenkins 
+docker network create jenkins
+
+# Create the Docker environment
+docker run --name jenkins-docker --rm --detach \
+  --privileged --network jenkins --network-alias docker \
+  --env DOCKER_TLS_CERTDIR=/certs \
+  --volume jenkins-docker-certs:/certs/client \
+  --volume jenkins-data:/var/jenkins_home \
+  --publish 2376:2376 docker:dind --storage-driver overlay2
